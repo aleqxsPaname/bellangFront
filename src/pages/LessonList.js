8b907c3e89a4607048e322a6f3daa44c9a5d8f65
alexisdebeauvais <alexisdebeauvais@gmail.com>
2021-03-27 23:32:27 +0100

    adding a fake filter (just as a reminder)

--- src/pages/LessonList.js
+++ src/pages/LessonList.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Lessons from ".././lessons/Lessons";
 import HeroSection from "../sections/HeroSection";
+import Filter from "../sections/Filter";
 
 function LessonList() {
   return (
     <>
       <HeroSection />
+      <Filter />
       <Lessons />
     </>
   );
--- src/sections/PurposeOfSite.js
+++ src/sections/PurposeOfSite.js
@@ -4,6 +4,8 @@ function PurposeOfSite() {
   return (
     <div>
       <h1>blablabla</h1>
+      <p>please click here to view our dialogue, perfect for a starter....</p>
+      <p>please click here to view our dialogues....</p>
     </div>
   );
 }
