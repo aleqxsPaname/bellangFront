a93c9bd198489418c44dfcda8a6496684ededf3b
alexisdebeauvais <alexisdebeauvais@gmail.com>
2021-03-28 00:27:43 +0100

    DIAPORAMA TEST adding a picture

---
+++ src/pages/Diaporama.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Diaporama() {
+  return (
+    <div>
+      <h1>Here the diaporama</h1>
+
+      <div className="row">
+        <div className="col">
+          <img
+            className="mg-fluid"
+            src="../images/img-2rs.png"
+            // src="../images/img-2.jpg"
+            //   style="width:100%; height:auto"
+            width="100%"
+            //   alt="placeholder 960"
+            //   class="img-responsive"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Diaporama;
---
+++ src/sections/Filter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Filter() {
+  return <div>FILTER BTWN : DIALOGUE / DIAPORAMA</div>;
+}
+
+export default Filter;
