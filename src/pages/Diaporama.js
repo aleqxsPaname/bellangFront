6b06be37f2e83c918b57a4ed293625189851c7f8
alexisdebeauvais <alexisdebeauvais@gmail.com>
2021-03-28 18:48:42 +0200

    DIAPORAMA TEST adjusting font-size to be responsive

--- src/pages/Diaporama.js
+++ src/pages/Diaporama.js
@@ -5,7 +5,7 @@ import "./diaporama.css";
 function Diaporama() {
   return (
     <div>
-      <h1>Here the diaporama</h1>
+      <h1>The Picnic Islands</h1>
       <div className="container-overlayed">
         <div className="row fluid">
           <img className="fluid" src="../images/img-2rs.png" width="100%" />
@@ -23,12 +23,11 @@ function Diaporama() {
           </div>
           <div className="bottom right overlay col-7">
             <span className="bottom right overlay">
-              The islands are a shelter for many species, such as the great
-              white shark...
+              But the islands are also a shelter for many species, such as the
+              great white shark...
             </span>
           </div>
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
